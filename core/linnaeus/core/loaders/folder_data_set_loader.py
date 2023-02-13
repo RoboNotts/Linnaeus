c8bffe4f7bee3af1f6cfaa2050ce757c035ca906
DTheLegend <wampamba.daudi@gmail.com>
2023-02-13 14:35:42 +0000

    Reduced inter batch file reads by caching

--- core/linnaeus/core/loaders/folder_data_set_loader.py
+++ core/linnaeus/core/loaders/folder_data_set_loader.py
@@ -16,19 +16,46 @@ class FolderDataSetLoader(Dataset):
         self.classes = classes
 
         # initialize a list to save input images as torch tensors
-        self._dataset = list(self.dataset_path.glob("*.xml"))
+        dataset = list(self.dataset_path.glob("*.xml"))
+
+        self._dataset = []
+
+        for label in dataset:
+            dom = xml_parse(str(label.resolve()))
+            # obtain root of the xml file
+            root = dom.documentElement
+
+            # Get image from provided path
+            image_path = root.getElementsByTagName('path')[0].childNodes[0].data
+            objects = root.getElementsByTagName("object")
+            # analyse annotations
+            tags = {c:[] for c in self.classes}
+            for obj in objects:
+                bndbox = obj.getElementsByTagName('bndbox')[0]
+                name = obj.getElementsByTagName('name')[0]
+                name_data = name.childNodes[0].data
+                
+                xmin = bndbox.getElementsByTagName('xmin')[0]
+                xmin_data = float(xmin.childNodes[0].data)
+                
+                ymin = bndbox.getElementsByTagName('ymin')[0]
+                ymin_data = float(ymin.childNodes[0].data)
+                
+                xmax = bndbox.getElementsByTagName('xmax')[0]
+                xmax_data = float(xmax.childNodes[0].data)
+                
+                ymax = bndbox.getElementsByTagName('ymax')[0]
+                ymax_data = float(ymax.childNodes[0].data)
+
+                # obtain top left anf right bottom coordinate
+                l = [xmin_data, ymin_data, xmax_data, ymax_data]
+                tags[name_data].append(l)
+            self._dataset.append({"image": (self.dataset_path / image_path).resolve(), "tags": tags})
 
     def __getitem__(self, index):
         # Return label of an image corresponding to its index.# read annotation file
         label = self._dataset[index]
-
-        dom = xml_parse(str(label.resolve()))
-        # obtain root of the xml file
-        root = dom.documentElement
-
-        # Get image from provided path
-        path = root.getElementsByTagName('path')[0].childNodes[0].data
-        image = cv2.imread(str((self.dataset_path / path).resolve()))
+        image = cv2.imread(str(label["image"]))
     
         # obtain image size
         row = image.shape[0]
@@ -36,34 +63,11 @@ class FolderDataSetLoader(Dataset):
         # image preprocessing
         torch_image = torch.from_numpy(np.transpose(image, (2, 0, 1)))
         torch_image = preprocessing(torch_image)
-        objects = root.getElementsByTagName("object")
-        # analyse annotations
-        tags = {c:[] for c in self.classes}
-        for obj in objects:
-            bndbox = obj.getElementsByTagName('bndbox')[0]
-            name = obj.getElementsByTagName('name')[0]
-            name_data = name.childNodes[0].data
-            
-            xmin = bndbox.getElementsByTagName('xmin')[0]
-            xmin_data = int(float(xmin.childNodes[0].data))
-            
-            ymin = bndbox.getElementsByTagName('ymin')[0]
-            ymin_data = int(float(ymin.childNodes[0].data))
-            
-            xmax = bndbox.getElementsByTagName('xmax')[0]
-            xmax_data = int(float(xmax.childNodes[0].data))
-            
-            ymax = bndbox.getElementsByTagName('ymax')[0]
-            ymax_data = int(float(ymax.childNodes[0].data))
 
-            # obtain top left anf right bottom coordinate
-            left = int(480 * xmin_data / col)
-            top = int(360 * ymin_data / row)
-            right = int(480 * xmax_data / col)
-            bottom = int(360 * ymax_data / row)
-            l = [left, top, right, bottom]
-            tags[name_data].append(l)
-        tag_list = [torch.Tensor([self.classes.index(k), *vv]) for k, v in tags.items() for vv in v if len(vv) > 0]
+        scalex = lambda x : int(x * 480 / col)
+        scaley = lambda y : int(y * 360 / row)
+
+        tag_list = [torch.Tensor([self.classes.index(k), scalex(vv[0]), scaley(vv[1]), scalex(vv[2]), scaley(vv[3])]) for k, v in label["tags"].items() for vv in v if len(vv) > 0]
         return torch_image, tag_list
 
     def __len__(self):
