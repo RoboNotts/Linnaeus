b9e463d14fb9f641e856f91e16900d35bd612b9d
dthelegend <wampamba.daudi@gmail.com>
2023-05-29 18:11:28 +0100

    jdja

--- linnaeus/linnaeus_ultima.py
+++ linnaeus/linnaeus_ultima.py
@@ -20,7 +20,7 @@ class LinnaeusUltima():
         results = self.yolo(img, *args, **kwargs)
         result_boxes = results[0].boxes
 
-        transformed_boxes = self.sam_predictor.transform.apply_boxes_torch(result_boxes.xyxy.cpu(), img.shape[:2])
+        transformed_boxes = self.sam_predictor.transform.apply_boxes_torch(result_boxes.xyxy.to(self.device), img.shape[:2])
 
         masks, _, _ = self.sam_predictor.predict_torch(
             point_coords=None,
